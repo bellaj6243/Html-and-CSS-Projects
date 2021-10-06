d95105edcb508ef0b0d1839d3d3dadd27ca7f677
bellaj6243 <91988863+bellaj6243@users.noreply.github.com>
2021-10-06 12:46:40 -0500

    Create Basic_HTML_and_Javascript_1.py

---
+++ Basic_HTML_and_Javascript_1.py
@@ -0,0 +1,5 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> alt>img>https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreepngimg.com%2Fpng%2F30403-kitten-image&psig=AOvVaw3Mw6to3fp8e_HmMvZcQ3L2&ust=1633628460894000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLDHtbSqtvMCFQAAAAAdAAAAABAD</img>
+SyntaxError: invalid syntax
+>>> <b><i><u>Paragraph:)</u></i></b>
